0d8f62c1c2716a1fc178b5ffde18c489731969f0
Takahashi Takuya <takahashitakuya031126@gmail.com>
2022-04-03 14:56:54 +0900

    create menu.php, footer.php and change index.php, manage-admin.php

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,4 @@
-<html>
-    <head>
-        <title>Food Order Website - Home Page</title>
-        <link rel="stylesheet" href="../css/admin.css">
-    </head>
-
-    <body>
-        <!-- Menu Section -->
-        <div class="menu text-center">
-            <div class="wrapper">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Admin</a></li>
-                    <li><a href="#">Category</a></li>
-                    <li><a href="#">Food</a></li>
-                    <li><a href="#">Order</a></li>
-                </ul>
-            </div>
-        </div>
+<?php include('partials/menu.php'); ?>
 
         <!-- Main Content Section -->
         <div class="main-content">
@@ -51,11 +33,4 @@
             </div>
         </div>
 
-        <!-- Footer Section -->
-        <div class="footer">
-            <div class="wrapper">
-                <p class="text-center">2021 All rights reserved, Some Restaurant. Developed By Takahashi Takuya</p>
-            </div>
-        </div>
-    </body>
-</html>
+<?php include('partials/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,8 @@
+        <!-- Footer Section -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All rights reserved, Food House. Developed By Takahashi Takuya</p>
+            </div>
+        </div>
+    </body>
+</html>
