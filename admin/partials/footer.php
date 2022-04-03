d2dfbe1980833855758aafd931abb01c328892b4
Takahashi Takuya <takahashitakuya031126@gmail.com>
2022-04-03 15:07:43 +0900

    create manage-category.php, manage-food.php, manage-order.php and change menu.php, footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
         <!-- Footer Section -->
         <div class="footer">
             <div class="wrapper">
-                <p class="text-center">2021 All rights reserved, Food House. Developed By Takahashi Takuya</p>
+                <p class="text-center">2022 All rights reserved, Food House. Developed By Takahashi Takuya</p>
             </div>
         </div>
     </body>
